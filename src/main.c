7486a420fe89a7534eff3dd83c542304eb068e54
= <=>
2015-12-20 18:42:09 +0200

    v1

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By:  <>                                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/20 15:03:30 by                   #+#    #+#             */
-/*   Updated: 2015/12/20 18:03:09 by                  ###   ########.fr       */
+/*   Updated: 2015/12/20 18:41:58 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,19 +28,54 @@ t_double_list		*read_list(int fd)
 	return (result);
 }
 
+int					calculate_choice(t_double_list *lst)
+{
+	int		index;
+
+	index = 0;
+	while (lst->next)
+	{
+		lst = lst->next;
+		index++;
+	}
+	if (!g_intent[index])
+		return (lst->value % 4);
+	else
+		return (lst->value % 4 - 1 > 0 ? lst->value % 4 - 1 : 1);
+}
+
+void				player_move(t_double_list *lst)
+{
+	int		choice;
+	char		*answer;
+
+	get_next_line(1, &answer);
+	choice = ft_atoi(answer);
+	lst->value -= choice;
+}
+
+void				my_move(t_double_list *lst)
+{
+	int		choice;
+
+	choice = calculate_choice(lst);
+	ft_putendl("I chose ");
+	ft_putnbr(choice);
+	ft_putendl("");
+	lst->value -= choice;
+}
+
 void				run_game(t_double_list *lst)
 {
-	char		*answer;
-	int			choice;
-
 	while (lst->next)
 	{
 		ft_putendl("--------------------------------------");
 		print_list(lst);
 		ft_putendl("How many sticks do you want to remove?");
-		get_next_line(1, &answer);
-		choice = ft_atoi(answer);
-		lst->value -= choice;
+		player_move(lst);
+		if (lst->value == 0)
+			lst = lst->next;
+		my_move(lst);
 		if (lst->value == 0)
 			lst = lst->next;
 	}

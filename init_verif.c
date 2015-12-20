115bd1342cebcb9cb409d58fdf800daeec9c972e
radugr7 <grumazescu_radu@yahoo.com>
2015-12-20 16:54:16 +0000

    da

--- init_verif.c
+++ init_verif.c
@@ -3,10 +3,13 @@ void	init(t_double_list **lst)
 	g_intent = (char*)malloc(list_size(lst));
 }
 
-int	verificare_tura(t_double_list *ls)
+void	verificare_tura(t_double_list *ls)
 {
-	int index = 0;
-	while (ls)
+	int index;
+	
+	index = 1;
+	g_intent[0] = 0;
+	while (ls->next)
 	{
 		g_intent[index++] = ls->value % 4 == 1;
 		ls = ls->next;
